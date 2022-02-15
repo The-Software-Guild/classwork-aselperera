64833658bf4bf7e98851987aae169becbcf8a437
aselperera <aselperera@hotmail.com>
2022-02-15 16:40:03 +0000

    Added gitignore, HelloWorld using NetBeans

---
+++ Day 7/02 HelloWorldNetBeans/src/main/java/ap/helloworld/Hello.java
@@ -0,0 +1,18 @@
+/*
+ *@author: Asel Perera
+ *email: aselperera@hotmail.com
+ *date: 2022/02/15
+ *purpose: My first Hello World program in an IDE
+ */
+package ap.helloworld;
+
+/**
+ *
+ * @author aselperera
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Asel!");
+    }
+}
