304032591f158763115ac2a5f871bec0ca5bb076
aselperera <aselperera@hotmail.com>
2022-02-17 14:49:28 +0000

    Practice programming #9 first and last questions

---
+++ Day 7/05 PracticeProgramming/PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/arrays/ARainbow.java
@@ -0,0 +1,20 @@
+package com.sg.foundations.flowcontrol.arrays;
+
+public class ARainbow {
+    public static void main(String[] args) {
+        String[] colors = {"Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"};
+
+//        System.out.println(colors[5]);
+//        System.out.println(colors[3]);
+//        System.out.println(colors[2]);
+//        System.out.println(colors[1]);
+//        System.out.println(colors[4]);
+//        System.out.println(colors[0]);
+//        System.out.println(colors[6]);
+        
+        for (int i = 0; i < colors.length; i++) {
+            System.out.println(colors[i]);
+        }
+
+    }
+}
